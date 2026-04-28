babfca9426f888c992e6b52f070c631cc10a64e2
programadorisgod <catapiascastro@gmail.com>
2026-04-28 09:27:46 -0500

    add adapter-node

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,29 +2,31 @@ module.exports = {
     apps: [
         {
             name: "pm2-view",
-            script: "node",
-            args: "node_modules/.bin/vite preview --host 0.0.0.0 --port 5179",
+            script: "./build/index.js",
             cwd: __dirname,
+
+            exec_mode: "fork",
             instances: 1,
+
             autorestart: true,
             watch: false,
             max_memory_restart: "500M",
+
             env: {
                 NODE_ENV: "production",
                 PORT: "5179",
-                HOST: "0.0.0.0",
                 ORIGIN: "http://localhost:5179",
                 BODY_SIZE_LIMIT: "Infinity",
             },
-            error_file: "./logs/pm2-view-error.log",
-            out_file: "./logs/pm2-view-out.log",
+
+            error_file: "./logs/error.log",
+            out_file: "./logs/out.log",
             merge_logs: true,
             log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+
             min_uptime: "10s",
             max_restarts: 10,
             restart_delay: 2000,
-            kill_timeout: 5000,
-            listen_timeout: 10000,
         },
     ],
 };
